3bbbba06c22a81b0b8173a055f71f9ff7779e5de
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-09 15:49:04 +0600

    navbar and sidebar created

--- portfolio-site-using-react-2/src/App.jsx
+++ portfolio-site-using-react-2/src/App.jsx
@@ -1,13 +1,25 @@
+//import Test from "./Test";
 import "./app.scss";
+//import Contact from "./components/contact/Contact";
+//import Cursor from "./components/cursor/Cursor";
+//import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+//import Parallax from "./components/parallax/Parallax";
+//import Portfolio from "./components/portfolio/Portfolio";
+//import Services from "./components/services/Services";
 
 const App = () => {
   return (
-  
     <div>
-    <section>Hero</section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Contact</section>
+      {/*<Cursor />*/}
+      <section id="Homepage">
+        <Navbar />
+        
+      </section>
+      
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
     </div>
   );
 };
