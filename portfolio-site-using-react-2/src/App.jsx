e365c866fd2ada6ad05accd5464d989d82f92383
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-09 17:49:50 +0600

    Parallax created

--- portfolio-site-using-react-2/src/App.jsx
+++ portfolio-site-using-react-2/src/App.jsx
@@ -4,7 +4,7 @@ import "./app.scss";
 //import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-//import Parallax from "./components/parallax/Parallax";
+import Parallax from "./components/parallax/Parallax";
 //import Portfolio from "./components/portfolio/Portfolio";
 //import Services from "./components/services/Services";
 
@@ -16,7 +16,15 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+         
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       {/* Framer Motion Crash Course */}
       {/* <Test/>
     <Test/> */}
