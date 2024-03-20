d78ddc110113973e438df43e532587b357414521
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-20 13:21:34 +0600

    contacts page outside the Root page

--- react-routing-explore/src/main.jsx
+++ react-routing-explore/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './error-page';
+import Root from "./routes/root";
+import Contact from "./routes/contact";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
+  <RouterProvider router={router} />
 
   </React.StrictMode>,
 )
